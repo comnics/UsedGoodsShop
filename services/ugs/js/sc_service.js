982668be5148abea1d6aa46652f2fd34e36c75ca
comnic <comnics@gmail.com>
2018-03-24 01:12:26 +0900

    Refactoring - 주석 및 click 이벤트 추가.

--- services/ugs/js/sc_service.js
+++ services/ugs/js/sc_service.js
@@ -4,6 +4,7 @@
 //$.getScript("js/sc_util.js");
 
 var site_log = new SiteLOG();
+
 if(_debug != "Y")
     site_log.hide();
 
@@ -17,6 +18,9 @@ App = {
     UsedGoodsShop: null,
     BDGToken: null,
 
+    /**
+     * ABI파일 로딩 후 initWeb3() -> initContract() -> initApp() 호출한다.
+     */
     init: function () {
         // Load pets.
         $.getJSON('contract/UsedGoodsShopABI.json', function (data) {
@@ -32,6 +36,10 @@ App = {
 
     },
 
+    /**
+     * web3 초기화 한다.
+     * @returns {*}
+     */
     initWeb3: function () {
 
             site_log.println("Web3 Init");
@@ -58,28 +66,46 @@ App = {
         return App.initContract();
     },
 
+    /**
+     * Token과 UGS contract를 초기화 한다.
+     */
     initContract: function(){
-           site_log.println("Create Contract : token, shop");
+        site_log.println("Create Contract : token, shop");
 
-            this.contractUGS = this.web3.eth.contract(this.sc_ugs_abi);
-            this.UsedGoodsShop = this.contractUGS.at(this.sc_ugs_address);
+        this.contractUGS = this.web3.eth.contract(this.sc_ugs_abi);
+        this.UsedGoodsShop = this.contractUGS.at(this.sc_ugs_address);
 
-            this.contractToken = this.web3.eth.contract(this.sc_token_abi);
-            this.BDGToken = this.contractToken.at(this.sc_token_address);
+        this.contractToken = this.web3.eth.contract(this.sc_token_abi);
+        this.BDGToken = this.contractToken.at(this.sc_token_address);
 
+        this.initApp();
+
+    },
+
+    /**
+     * App을 초기화 한다.
+     *  updateBalance()
+     *  getAccount()
+     *  등록상품수를 구한 뒤 getProductList()를 호출한다. : TODO: 개선해야함.
+     */
+    initApp: function(){
         this.updateBalance();
         this.getAccount();
 
         this.UsedGoodsShop.productCnt.call((err, res) => {
             if(!err){
-                p_cnt = res;
-                if(p_cnt > 0)
-                    this.getProductList(p_cnt);
+            p_cnt = res;
+            if(p_cnt > 0)
+                this.getProductList(p_cnt);
             }
         });
 
     },
 
+    /**
+     * UGS 컨트랙트가 가지고 있는 Token의 잔고를 구한다.
+     * 이는 구매등을 통해 컨트랙트에 escrow개념으로 머물러 있는 Token이다.
+     */
     updateBalance: function() {
         site_log.println("Get contract token balance");
 
@@ -98,6 +124,10 @@ App = {
 
     },
 
+    /**
+     * 상품의 개수 만큼 가져와서 화면에 출력한다.
+     * @param p_cnt
+     */
     getProductList: function(p_cnt){
         site_log.println("Get Products Info from Smart Contract");
 
@@ -149,6 +179,9 @@ App = {
 
     },
 
+    /**
+     * Account를 구하고 첫번째 account를 default_account로 지정한다.
+     */
     getAccount: function(){
         web3.eth.getAccounts(function(error, accounts) {
             if(!error) {
@@ -164,6 +197,96 @@ App = {
 };
 
 $(function() {
-       App.init();
+    App.init();
+
+    /**
+     * click 이벤트를 정의 한다.
+     */
+
+    $("#productRegBtn").click(function(){
+
+        var p_name = $("#regProductName").val();
+        var p_summary = $("#regProductSummary").val();
+        var p_price = $("#regProductPrice").val();
+        var p_image = $("#regProductImage").val();
+
+        if(p_name == "" || p_summary == "" || p_price == ""){
+            alert('상품 등록을 위한 값을 모두 입력하셔야 합니다.');
+
+            return false;
+        }
+
+        const transactionObject = {
+            from: cAccounts[0],
+            gas: web3.toHex(300000),
+            gasPrice: gas_price
+        };
+
+        site_log.println("Product Registration : " + p_name);
+
+        App.UsedGoodsShop.regProduct.sendTransaction(p_name, p_price, p_summary, p_image, transactionObject, (err, res) => {
+            if(!err){
+                console.log(res);
+                site_log.println("Product Registration Tx info : <a href=\"https://testnet.etherscan.io/tx/" + res + "\" target=\"_blank\">" + res + "</a>");
+            }
+        });
+
+        App.UsedGoodsShop.RegProduct().watch(function(err, res){
+            if(!err){
+                console.log(res);
+                site_log.println("Callback Product Registration Tx info Start >> ");
+                site_log.println("address : " + res.address);
+                site_log.println("blockHash : " + res.blockHash);
+                site_log.println("blockNumber : " + res.blockNumber);
+                site_log.println("transactionHash : " + res.transactionHash);
+                site_log.println("address : " + res.address);
+
+                alert('등록되었습니다. 제품 등록까지 최대 수분이 소요될 수 있습니다.');
+            }
+        });
+
+    });
+
+
+    $("#productList").on("click", ".btn-product-buy", function(){
+
+        var p_id = $(this).data("product-id");
+        var p_seller = $(this).data("product-seller");
+        var p_price = $(this).data("product-price");
+
+        const transactionObject = {
+            from: cAccounts[0],
+            gas: web3.toHex(300000),
+            gasPrice: gas_price
+        };
+
+        site_log.println("Buy Product : " + p_id);
+
+        App.UsedGoodsShop.buy.sendTransaction(p_id, transactionObject, function(err, res){
+            if(!err){
+                console.log(res);
+                site_log.println("Buy Product Tx info : <a href='https://ropsten.etherscan.io/tx/"+ res + "' target='_blank'>" + res + "</a>");
+
+                alert('상품 구입 Tx이 생성되었습니다.');
+
+            }else{console.log(err); alert('상품 구입에 실패 했습니다. 잠시 후 다시 이용해 주세요!');}
+        });
+
+        App.UsedGoodsShop.BuyProduct().watch(function(err, res){
+            if(!err){
+                console.log(res);
+                site_log.println("Callback Buy Product Tx info Start >> ");
+                site_log.println("address : " + res.address);
+                site_log.println("blockHash : " + res.blockHash);
+                site_log.println("blockNumber : " + res.blockNumber);
+                site_log.println("transactionHash : " + res.transactionHash);
+                site_log.println("address : " + res.address);
+
+                alert('상품 구입 요청이 처리 되었습니다.\n상품 수령 후 꼭 구매완료 버튼을 클릭해 주세요!');
+            }
+
+        });
+    });
+
 });
 
